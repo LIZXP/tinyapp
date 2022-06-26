a6dec4d51bc595ab170e8836d7c1bb9411671ee7
LIZXP <Peter.xli@outlook.com>
2022-06-25 23:57:54 -0600

    dding a Second Route and Template

--- express_server.js
+++ express_server.js
@@ -12,15 +12,6 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 }); //set the main tag
 
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
-
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 }); // this will turns the object into json file
@@ -33,6 +24,14 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 }); //set the urls tag and have the ejs render in the views folder
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log("Server is listening on PORT: ", PORT);
