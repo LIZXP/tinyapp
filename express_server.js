c5e87b542413804cf7bae9ac2f001babdaf82e46
LIZXP <Peter.xli@outlook.com>
2022-06-25 23:45:00 -0600

    add some notes

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 app = express();
 PORT = 8080;
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //set the ejs as view engine
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -10,7 +10,7 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
+}); //set the main tag
 
 // app.get("/set", (req, res) => {
 //   const a = 1;
@@ -23,17 +23,17 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});
+}); // this will turns the object into json file
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+}); //set the hello tag
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-});
+}); //set the urls tag and have the ejs render in the views folder
 
 app.listen(PORT, () => {
   console.log("Server is listening on PORT: ", PORT);
-});
+}); // start the server and listen to the PORT we set as 8080
